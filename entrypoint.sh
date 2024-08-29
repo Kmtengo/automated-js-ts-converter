ea6fa4e58fad8b32f62245fb69f335846ac2fad1
Kmtengo <j12-4195-2019@student.mksu.ac.ke>
2024-08-29 17:56:44 +0300

    Create the entrypoint.sh file

---
+++ entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "====================="
+
+# Git configuration commands
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${GITHUB_ACTOR}"@localhost
+
+# Git command for ensuring we can run workflows
+git config --global --add safe.directory /github/workspace
+
+# switching to the source branch of the pull request that triggered the GitHub Actions workflow.
+git checkout "${GITHUB_HEAD_REF}"
+
+# Run JavaScript to TypeScript conversion
+npx convert-js-to-ts -p src/**/*.jsx --outputDir=src/output
+
+# Check if any TypeScript files were generated
+if [ "$(ls -A src/output/*.ts 2>/dev/null)" ]; then
+  # Run tests
+  npm test
+else
+  echo "No TypeScript files were generated. Exiting..."
+  exit 1
+fi
+
+git add -A && git commit -m "Convert JS files to TS"
+git push -U origin main
+
+# Comment on PR
+# This assumes that you have the `gh` tool installed and authenticated
+gh auth login --with-token ${{ secrets.GITHUB_TOKEN }}
+gh pr comment ${{ github.event.number }} --body "JS files have been converted to TS and tests have been run successfully. Please review the changes."
+
+# Assign reviewer
+gh pr review --assignee 'Paul-Sanga'
+
+echo "====================="
